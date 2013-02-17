87e4a4bc306d2561e9560a65148c413248e11b4a
Chris Taylor <public_github@shockwaver.org>
2013-02-17 15:55:16 -0600

    bugs

--- plugins/siriproxy-flexget/lib/siriproxy-flexget.rb
+++ plugins/siriproxy-flexget/lib/siriproxy-flexget.rb
@@ -15,7 +15,7 @@ class SiriProxy::Plugin::Flexget < SiriProxy::Plugin
     #if you have custom configuration options, process them here!
   end
 
-	def flexget(command, movie)
+	def flexget(movie, command="download") # default to download
 		#download
 		if(command =~ "download")
 			# add movie to queue
@@ -56,7 +56,7 @@ class SiriProxy::Plugin::Flexget < SiriProxy::Plugin
     if(response =~ /yes/i) # sounds good, lets download it
 		# sleep 5
 		say "Ok! I will download #{movie}"
-		flexget("download", movie)
+		flexget(movie)
 		# download code here
 		# output = `su - pi -c "flexget --movie-queue add \\"#{movie}\\""`
 		# sleep 5
